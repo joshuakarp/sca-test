13941881ae37cab40e2927c1d41cbffe542f66ec
Joshua Karp <joshua.karp@matrix.ai>
2022-02-13 12:32:42 +1100

    Returning correct structure of episodes from RSS feed

--- src/controllers/houstonController.js
+++ src/controllers/houstonController.js
@@ -1,14 +1,25 @@
 import Parser from 'rss-parser';
+import { convertISODateToAEST } from '../utilities/format-iso-date-to-AEST.js';
 
 class HoustonController {
   async getPodcasts(count = 10) {
     const parser = new Parser();
     const feed = await parser.parseURL('https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss');
 
+    // Structure the episodes as required
+    const requiredEpisodes = feed.items.splice(0, count);
+    const parsedEpisodes = [];
+    requiredEpisodes.forEach((episode) => {
+      parsedEpisodes.push({
+        title: episode.title,
+        audioUrl: episode.enclosure.url,
+        publishedDate: convertISODateToAEST(episode.isoDate),
+      })
+    });
     const payload = {
       title: feed.title,
       description: feed.description,
-      episodes: feed.items.splice(0, count),
+      episodes: parsedEpisodes,
     };
     // Return only the first 10 podcasts
     return payload;
